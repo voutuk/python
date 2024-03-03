12829a0e785d0181b42212d9ee1855d449b1fd30
Your Name <vout@ex.id>
2024-03-03 15:19:17 +0200

    1

--- hello.py
+++ hello.py
@@ -7,14 +7,14 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(commands=['exec'])
 def execute_command(message):
     print (message.chat.id)
-    if (message.chat.id == 650550237 or message.chat.id == 5026461524):
+    if (message.chat.id == 650550237):
         try:
             result = subprocess.getoutput(message.text[6:])
             bot.reply_to(message, "Result:\n" + result)
         except Exception as e:
             bot.reply_to(message, "An error occurred:\n" + str(e))
     else:
-        bot.reply_to("id !=")
+         bot.send_message(message.chat.id, "!=")
     
 
 bot.polling()
