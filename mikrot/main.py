b05f15171d460a5ad0b2c751e33f6b65f5e28ad6
STEP PC <you@example.com>
2024-03-31 17:07:18 +0300

    1

--- mikrot/main.py
+++ mikrot/main.py
@@ -1,5 +1,5 @@
-import paramiko, subprocess
-from secrets import username, password
+import paramiko, subprocess, telebot
+from secrets import username, token
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect(hostname='10.10.13.123', port=2255, username=username, key_filename="paramiko")
